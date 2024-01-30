0b1bbf93facbb51d8ab071429c0f8b11ef545ebb
Pavlina Ivanova <pavlina33331988@gmail.com>
2024-01-30 21:57:03 +0200

    added
    added

---
+++ app.js
@@ -0,0 +1,10 @@
+﻿const tl = gsap.timeline({default: {ease: 'power2.out'}})
+/* tl.to(".slider", { y: "-100", duration: 1.2 });
+tl.to(".intro .hero", {y:"0%", duration:1.2}, "-=1"); */
+tl.fromTo(".nav", {opacity:0}, {opacity:1, duration:1});
+
+
+
+tl.to(".slider", {rotation:0, y: "-100", duration: 1.2 });
+tl.to(".intro .hero", { rotation: 0, y: 100, duration: 1.2 });
+tl.to(".slider", {rotation: 360, duration: 5, ease: "elastic"});
